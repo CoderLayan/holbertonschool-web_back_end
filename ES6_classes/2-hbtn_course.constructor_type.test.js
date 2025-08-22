e314345473d84fc9ae291085677fb96530883726
layan <layanazez@gmail.com>
2025-08-22 12:53:39 +0000

    fix 2

--- ES6_classes/2-hbtn_course.constructor_type.test.js
+++ ES6_classes/2-hbtn_course.constructor_type.test.js
@@ -1,27 +1,13 @@
-// Test the implementation
 import HolbertonCourse from "./2-hbtn_course.js";
 
-// Test valid creation
-const course = new HolbertonCourse("ES6", 1, ["Bob", "Jane"]);
-console.log('Valid course:', course);
-
-// Test invalid name
-try {
-  new HolbertonCourse(10, 20, ["Lucie", "Guillaume"]);
-} catch (err) {
-  console.log('Error (name):', err.message); // "Name must be a string"
-}
-
-// Test invalid length
-try {
-  new HolbertonCourse('PHP', '20', ["Lucie", "Guillaume"]);
-} catch (err) {
-  console.log('Error (length):', err.message); // "Length must be a number"
-}
-
-// Test invalid students
-try {
-  new HolbertonCourse('PHP', 20, "not an array");
-} catch (err) {
-  console.log('Error (students):', err.message); // "Students must be an array of strings"
-}
+describe("HolbertonCourse checks constructor types", () => {
+  test("should throw errors for invalid types", () => {
+    expect(() => {
+      new HolbertonCourse(10, 20, ["Lucie", "Guillaume"]);
+    }).toThrow('Name must be a string');
+    
+    expect(() => {
+      new HolbertonCourse('PHP', '20', ["Lucie", "Guillaume"]);
+   }).toThrow('Length must be a number');
+  });
+});
